8b3a2fb922f775433e75ba999689fa74e7785d86
udcsk <dach.vo.ov.9612@gmail.com>
2016-07-08 15:53:50 +0900

    本文の変化に対応（テキストボックスの入力内容を素直に解析に投げてます）。

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
   $(function() {
     $("#sendText").click(function(){
       var reqestUrl = "http://jlp.yahooapis.jp/KeyphraseService/V1/extract?appid=dj0zaiZpPU1XMmlaWE5YQmJrZiZzPWNvbnN1bWVyc2VjcmV0Jng9YzI-&sentence=";
-      var contentText = "だいちゃんはおっぱいが大好き";
+      var contentText = document.getElementById("textContent").value;
       contentText = encodeURI(contentText)
       var outputOption = "&output=json&callback=resultByYahoo";
       var resultByYahoo = "";
 
       console.log("コンソールログに出力", reqestUrl + contentText);
       //document.location = reqestUrl + contentText + outputOption;
+      //とりあえずテキストボックスから持ってきたのを解析投げてその結果をログに吐く。
     });
 });
